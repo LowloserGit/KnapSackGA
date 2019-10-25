6208d3033b90abf997d3a02892fd1597cc598e4a
LowloserGit <98miwaa@gmail.com>
2019-10-25 11:51:04 +0200

    Added files to project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KnapsackGA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(rnd.Next(1, 11));
+            }
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
