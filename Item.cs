4f33dee6ea350fc74b5c734c3b1f35858500c816
LowloserGit <98miwaa@gmail.com>
2019-10-28 11:27:00 +0100

    Added Item class. Added variables and the begining of inital solution

---
+++ Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnapsackGA
+{
+    class Item
+    {
+        public int weight { get; set; }
+        public int value{ get; set; }
+
+        public Item(int weight, int value)
+        {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -7,15 +7,59 @@ namespace KnapsackGA
         static void Main(string[] args)
         {
             var rnd = new Random();
+            int maxWeight = 30;
+            int amountOfObjects = 20;
+            int generationSize = 4;
+            int[] knapSack = new int[amountOfObjects];
+            Item[] objects = new Item[amountOfObjects];
+            int[][] nextGeneration = new int[generationSize][];
+            int[][] population = new int[2*generationSize][];
 
-            for (int i = 0; i < 100; i++)
+            //creating the available objects
+            for (int i = 0; i < objects.Length; i++)
             {
-                Console.WriteLine(rnd.Next(1, 11));
+                //Item myItem = new Item(rnd.Next(1, 11), rnd.Next(1, 100));
+                objects[i] = new Item(rnd.Next(1, 11), rnd.Next(1, 100));
+                Console.WriteLine("Weight: {0}", objects[i].weight);
+                Console.WriteLine("Value: {0}", objects[i].value);
+            }
+
+            //creating initial solution
+            for (int i = 0; i < nextGeneration.Length; i++)
+            {
+                for (int j = 0; j < knapSack.Length; j++)
+                {
+                    knapSack[j] = rnd.Next(0, 2);
+                }
+                nextGeneration[i] = knapSack;
             }
             
-            
-            
+
+
+
             Console.ReadLine();
+        }
+
+        static int calculateValue(int[] knapSack, Item[] objects, int maxWeight)
+        {
+            int currentValue;
+            int currentWeight;
+            int totalValue = 0;
+            int totalWeight = 0;
+            for (int i = 0; i < knapSack.Length; i++)
+            {
+                if (knapSack[i] == 1 && (totalWeight+objects[i].weight) < maxWeight)
+                {
+                    totalValue = totalValue + objects[i].value;
+                    totalWeight = totalWeight + objects[i].weight;
+                }
+                else
+                {
+                    //remove excess objects from knapsack
+                }
+            }
+
+            return 0;
         }
     }
 }
